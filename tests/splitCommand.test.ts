a3550085222e95d35c35987c11d45b477138435e
miguelkjesus <miguelkjesus05@gmail.com>
2024-01-14 17:29:42 +0000

    Added splitCommand() unit test

---
+++ tests/splitCommand.test.ts
@@ -0,0 +1,37 @@
+import { splitCommand } from "../src/splitCommand";
+
+type ValidTestCase = [
+  input: string,
+  expected: { command: string; args: string[] }
+];
+type ErrorTestCase = [input: string];
+
+describe("splitCommand()", () => {
+  let validInputs: ValidTestCase[] = [
+    ["command", { command: "command", args: [] }],
+    ["command   ", { command: "command", args: [] }],
+    ["command arg0 arg1", { command: "command", args: ["arg0", "arg1"] }],
+    [
+      "command   arg0   arg1   ",
+      { command: "command", args: ["arg0", "arg1"] },
+    ],
+    [" arg0 arg1", { command: "", args: ["arg0", "arg1"] }],
+    ["    arg0    arg1   ", { command: "", args: ["arg0", "arg1"] }],
+    ['command " arg 0 "', { command: "command", args: [" arg 0 "] }],
+  ];
+
+  it.each(validInputs)("correctly splits: %s", (input, expected) => {
+    let result = splitCommand(input);
+    expect(result).toHaveProperty("command");
+    expect(result.command).toEqual(expected.command);
+
+    expect(result).toHaveProperty("args");
+    expect(result.args).toEqual(expected.args);
+  });
+
+  let errorInputs: ErrorTestCase[] = [['command " arg0 " arg1 "arg 2']];
+
+  it.each(errorInputs)("throws on the input: %s", (input) => {
+    expect(() => splitCommand(input)).toThrow();
+  });
+});
